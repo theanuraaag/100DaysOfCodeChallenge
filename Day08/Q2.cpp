4baa8dea768cf9295abc559708a29fa866deda8e
theanuraaag <86941924+theanuraaag@users.noreply.github.com>
2023-06-11 20:14:31 +0530

    Create Q2.cpp

---
+++ Day08/Q2.cpp
@@ -0,0 +1,25 @@
+// https://leetcode.com/problems/remove-element/description/
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val)
+     {
+        int n = nums.size();
+        int nsize = 0;
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i]==val)
+                continue;
+            nums[nsize++] = nums[i];
+        }
+        return nsize;
+    }
+};
+
+/* Complexity
+    Time complexity:
+        O(n)
+
+    Space complexity:
+        O(1)
+*/

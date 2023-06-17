106b5152cb9fb8ed944fe54465ee7b9210db1d05
theanuraaag <86941924+theanuraaag@users.noreply.github.com>
2023-06-17 22:20:00 +0530

    Create Q3.cpp

---
+++ Day14/Q3.cpp
@@ -0,0 +1,31 @@
+// https://leetcode.com/problems/missing-number/description/
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int arr[10000]={0};
+        int i=0;
+        std::vector<int>::iterator itr=nums.begin();
+        while(itr!=nums.end())
+        {
+            arr[*itr]++;
+            itr++;
+            i++;
+        }
+        while(i>=0)
+        {
+            if(arr[i]==0)
+                break;
+            i--;
+        }
+        return i;
+    }
+};
+
+/* Complexity
+    Time complexity:
+        O(n)
+
+    Space complexity:
+        O(1)
+*/

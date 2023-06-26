4dc93a744aa8dc8a4ce408f16f900ee408ed1954
theanuraaag <86941924+theanuraaag@users.noreply.github.com>
2023-06-26 22:04:00 +0530

    Create Q4.cpp

---
+++ Day23/Q4.cpp
@@ -0,0 +1,50 @@
+// https://practice.geeksforgeeks.org/problems/palindrome-string0817/1
+
+//{ Driver Code Starts
+ 
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	int isPal(int i, string&S){
+	    
+        if(i>=S.length()/2) return 1;
+        if(S[i]!=S[S.length()-i-1]) return 0;
+        return isPal(i+1,S);
+    }
+	
+	int isPalindrome(string S)
+	{
+	    // Your code goes here
+	    return isPal(0,S);
+	}
+
+};
+
+//{ Driver Code Starts.
+
+int main() 
+{
+   	ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+   
+   	int t;
+   	cin >> t;
+   	while(t--)
+   	{
+   		string s;
+   		cin >> s;
+
+   	    Solution ob;
+
+   		cout << ob.isPalindrome(s) << "\n";
+   	}
+
+    return 0;
+}
+// } Driver Code Ends

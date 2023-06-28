73429fc6536cf270c68e2cf3e906ee3fa27af8bf
theanuraaag <86941924+theanuraaag@users.noreply.github.com>
2023-06-28 21:42:14 +0530

    Update Q1.cpp

--- Day25/Q1.cpp
+++ Day25/Q1.cpp
@@ -1,50 +1,123 @@
 // https://practice.geeksforgeeks.org/problems/maximum-depth-of-binary-tree/1
 
 //{ Driver Code Starts
-//Initial Template for C++
-
-
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
+#define MAX_HEIGHT 100000
 
+// Tree Node
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+};
 
+// Utility function to create a new Tree Node
+Node* newNode(int val) {
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+
+    return temp;
+}
+
+// Function to Build Tree
+Node* buildTree(string str) {
+    // Corner Case
+    if (str.length() == 0 || str[0] == 'N') return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for (string str; iss >> str;) ip.push_back(str);
+
+    // Create the root of the tree
+    Node* root = newNode(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if (currVal != "N") {
+
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= ip.size()) break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if (currVal != "N") {
+
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
 
 // } Driver Code Ends
-//User function Template for C++
-void swaap(int l, int r, string&str)
+/* The Node structure is
+struct Node
 {
-    if(l>=r) return;
-    swap(str[l],str[r]);
-    swaap(l+1,r-1,str);
-}
+    int data;
+    Node* left;
+    Node* right;
+}; */
 
-string reverseWord(string str){
-    
-  //Your code here
-  int i=0;
-  int j=str.length();
-  swaap(i,(j-1),str);
-  return str;
-}
-
+class Solution{
+  public:
+    /*You are required to complete this method*/
+    int maxDepth(Node *root) {
+        // Your code here
+        if(root == NULL) return 0;
+        else return (max(maxDepth(root->left),maxDepth(root->right))+1);
+    }
+};
 
 
 //{ Driver Code Starts.
-
 int main() {
-	
-	int t;
-	cin>>t;
-	while(t--)
-	{
-	string s;
-	cin >> s;
-	
-	cout << reverseWord(s) << endl;
-	}
-	return 0;
-	
+    int t;
+    string tc;
+    getline(cin, tc);
+    t = stoi(tc);
+    while (t--) {
+        string s, ch;
+        getline(cin, s);
+        Node* root = buildTree(s);
+        // getline(cin, ch);
+        Solution obj;
+        cout << obj.maxDepth(root) << endl;
+        // cout<<"~"<<endl;
+    }
+    return 0;
 }
-
 
 // } Driver Code Ends

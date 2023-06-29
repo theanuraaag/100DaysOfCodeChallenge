ce4aab603cd23024918a3efaab2c861bed040587
theanuraaag <86941924+theanuraaag@users.noreply.github.com>
2023-06-29 21:48:28 +0530

    Create Q1.cpp

---
+++ Day26/Q1.cpp
@@ -0,0 +1,39 @@
+// https://practice.geeksforgeeks.org/problems/next-happy-number4538/1
+
+class Solution{
+public:
+    int nextHappy(int N){
+        N++;
+        while(isHappy(N)!=true){
+            N++;
+        }
+        return N;
+    }
+
+    int isHappy(int number){
+        set<int> st;
+        while(1){
+            number = sumOfSquareDigits(number);
+            if(number==1){
+                return true;
+            }
+            if(st.find(number) != st.end()){
+                return false;
+            }
+            st.insert(number);
+        }
+    }
+
+    int sumOfSquareDigits(int number) {
+        int sum = 0;
+
+        while (number > 0) {
+            int digit = number % 10;
+            sum += digit * digit;
+            number /= 10;
+        }
+
+        return sum;
+    }
+};
+

579d4eed3dfa367c9985074070ab8bc18b89901f
theanuraaag <86941924+theanuraaag@users.noreply.github.com>
2023-07-05 21:50:10 +0530

    Create Q1.cpp

---
+++ Day32/Q1.cpp
@@ -0,0 +1,19 @@
+// https://practice.geeksforgeeks.org/problems/stock-buy-and-sell2615/1
+
+class Solution
+{
+public:
+    int stockBuyAndSell(int n, vector<int> &prices)
+    {
+        // code here
+        int profit = 0;
+        for (int i = 1; i < n; i++)
+        {
+            if (prices[i] > prices[i - 1])
+            {
+                profit += prices[i] - prices[i - 1];
+            }
+        }
+        return profit;
+    }
+};
